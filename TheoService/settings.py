06645cb445109359b77fe6e37d93b875a74cbb94
MHDSAT <gayemoha2001prime@gmail.com>
2022-02-15 05:05:19 +0000

    ok

--- TheoService/settings.py
+++ TheoService/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = '-oaqd!8ovofn*&hgww-o%43gc_x2)%ot0#%w1!5u6o!n35jj+o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com','.io']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com',".com"]
 
 
 # Application definition
